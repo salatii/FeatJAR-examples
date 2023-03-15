c9ef4a66c71b22568066d21cc26910a63463852e
kh24 <katjana-1.herbst@uni-ulm.de>
2023-03-15 22:08:41 +0100

    add assignment to simple example

--- src/main/java/de/featjar/examples/anlysis/SimpleExample.java
+++ src/main/java/de/featjar/examples/anlysis/SimpleExample.java
@@ -6,12 +6,17 @@ import de.featjar.base.extension.ExtensionManager;
 import de.featjar.formula.analysis.bool.BooleanSolution;
 import de.featjar.formula.analysis.bool.ComputeBooleanRepresentationOfCNFFormula;
 import de.featjar.formula.analysis.sat4j.ComputeSolutionSAT4J;
+import de.featjar.formula.analysis.value.ValueAssignment;
 import de.featjar.formula.io.FormulaFormats;
 import de.featjar.formula.structure.formula.IFormula;
 import de.featjar.formula.transformer.ComputeCNFFormula;
 import de.featjar.formula.transformer.ComputeNNFFormula;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 import static de.featjar.base.computation.Computations.*;
 
@@ -19,12 +24,11 @@ public class SimpleExample {
     protected static final ExtensionManager extensionManager = new ExtensionManager();
 
     public static void main(String[] args) {
-        //TODO Value Assignment for partial config
-
         // load formula (cnf) of choosen featuremodel
         File file = new File(SimpleExample.class.getClassLoader().getResource("featuremodels/basic.xml").getPath());
         IFormula formula = Commands.loadFile(file.getPath(), extensionManager.getExtensionPoint(FormulaFormats.class).get()).orElseThrow();
-
+        String config = loadConfiguration(SimpleExample.class.getClassLoader().getResource("featuremodels/basic.txt").getPath());
+        ValueAssignment assignment = parseConfig(config);
         // transform formula into boolean representation for further calculations
         var booleanRepresentation = async(formula)
                 .map(ComputeNNFFormula::new)
@@ -32,11 +36,40 @@ public class SimpleExample {
                 .map(ComputeBooleanRepresentationOfCNFFormula::new);
         // get boolean clause list from boolean representation
         var booleanClauseList = getKey(booleanRepresentation);
-
         // create HasSolution analyse with boolean clause list
-        var hasSolutionAnalyze = new ComputeSolutionSAT4J(booleanClauseList);
+        var analysis = new ComputeSolutionSAT4J(booleanClauseList);
+        // set assignment for partial config
+        var variableMap = getValue(booleanRepresentation);
+        analysis.setAssumedAssignment(assignment.toBoolean(variableMap));
         // compute result of analyze
-        ComputePresence<BooleanSolution> result = new ComputePresence<>(hasSolutionAnalyze);
+        ComputePresence<BooleanSolution> result = new ComputePresence<>(analysis);
         System.out.println(!result.computeResult().get());
+    }
+
+    public static String loadConfiguration(String filepath) {
+        File file = new File(filepath);
+        String result = "";
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine())
+                result += sc.nextLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        result = result.replaceAll(" ", "");
+        return result;
+    }
+
+    static public ValueAssignment parseConfig(String config) {
+        String[] configArr = config.split(",");
+        LinkedHashMap<String, Object> variableValuePairs = new LinkedHashMap<>();
+        Arrays.stream(configArr).forEach(entry -> {
+            if(entry.charAt(0) == '-') {
+                variableValuePairs.put(entry.substring(1), false);
+            } else {
+                variableValuePairs.put(entry, true);
+            }
+        });
+        return new ValueAssignment(variableValuePairs);
     }
 }
